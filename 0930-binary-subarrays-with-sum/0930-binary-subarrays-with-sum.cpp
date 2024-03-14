5dc58c1a57a477d411b36fc244d3e9fe48bb9e67
Rohit Kumar Mahato <95642350+Rohitkr2510@users.noreply.github.com>
2024-03-14 15:23:13 +0530

    Time: 18 ms (90.15%), Space: 31 MB (85.39%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,25 +1,34 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        int totalCount = 0;
+        int start = 0;
+        int prefixZeros = 0;
         int currentSum = 0;
-        // {prefix: number of occurrence}
-        unordered_map<int, int> freq; // To store the frequency of prefix sums
-
-        for (int num : nums) {
-            currentSum += num;
-            if (currentSum == goal){
-                totalCount++;
+        int totalCount = 0;
+        
+        // Loop through the array using end pointer
+        for (int end = 0; end < nums.size(); ++end) {
+            // Add current element to the sum
+            currentSum += nums[end];
+            
+            // Slide the window while condition is met
+            while (start < end && (nums[start] == 0 || currentSum > goal)) {
+                if (nums[start] == 1) {
+                    prefixZeros = 0;
+                } else {
+                    prefixZeros += 1;
+                }
+                
+                currentSum -= nums[start];
+                start += 1;
             }
-
-            // Check if there is any prefix sum that can be subtracted from the current sum to get the desired goal
-            if (freq.find(currentSum - goal) != freq.end()){
-                totalCount += freq[currentSum - goal];
+            
+            // Count subarrays when window sum matches the goal
+            if (currentSum == goal) {
+                totalCount += 1 + prefixZeros;
             }
-
-            freq[currentSum]++;
         }
-
+        
         return totalCount;
     }
 };
